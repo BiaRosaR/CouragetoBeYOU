006fd73b5e8944b107cec2d9eb294d8688acc06b
beatriz <beatriz.rocha@sptech.school>
2023-10-29 21:04:50 -0300

    refactor(dashboard.html): todos os gráficos funcionais

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -1,78 +1,24 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(idAquario, limite_linhas) {
-
-    instrucaoSql = ''
-
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc`;
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        momento,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico
-                    from medida
-                    where fk_aquario = ${idAquario}
-                    order by id desc limit ${limite_linhas}`;
-    } else {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-    }
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
-
-function buscarMedidasEmTempoReal(idAquario) {
-
-    instrucaoSql = ''
-
-    if (process.env.AMBIENTE_PROCESSO == "producao") {
-        instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc`;
-
-    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${idAquario} 
-                    order by id desc limit 1`;
-    } else {
-        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
-        return
-    }
-
-    console.log("Executando a instrução SQL: \n" + instrucaoSql);
-    return database.executar(instrucaoSql);
-}
 
 function chart1() {
-    const instrucao = `select sum(qtdBrancas) As totalBlusaBranca, sum(qtdPretas) AS totalBlusaPreta, sum(qtdAzuis) AS totalBlusaAzul  FROM Pesquisas;`;
+    const instrucao = `SELECT * FROM vw_pesquisaBlusas;`;
     return database.executar(instrucao);
 }
 
-// function chart2(){
-//     const instrucao = `select sum()`;
-// }
+function chart2(){
+    const instrucao = `SELECT * FROM vw_pequisasXadrez;`;
+    return database.executar(instrucao);
+}
+
+function chart3(){
+    const instrucao = `SELECT * FROM vw_preferenciaSapato;`;
+    return database.executar(instrucao);
+}
 
 
 module.exports = {
-    buscarUltimasMedidas,
-    buscarMedidasEmTempoReal,
-    chart1
+    chart1,
+    chart2,
+    chart3
 }
--- site/src/routes/medidas.js
+++ site/src/routes/medidas.js
@@ -3,21 +3,19 @@ var router = express.Router();
 
 var medidaController = require("../controllers/medidaController");
 
-router.get("/ultimas/:idAquario", function (req, res) {
-    medidaController.buscarUltimasMedidas(req, res);
-});
-
-router.get("/tempo-real/:idAquario", function (req, res) {
-    medidaController.buscarMedidasEmTempoReal(req, res);
-})
 
 router.get("/chart1", function (req, res) {
     medidaController.chart1(req, res);
 })
 
-// router.get("/chart2", function (req, res) {
-//     medidaController.chart2(req, res);
-// })
+router.get("/chart2", function (req, res) {
+    medidaController.chart2(req, res);
+})
+
+router.get("/chart3", function (req, res) {
+    medidaController.chart3(req, res);
+})
+
 
 
 module.exports = router;
